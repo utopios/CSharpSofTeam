fefd8151fd99ce3f35ef3f1b3b4d78d29ef713f0
PetitVulcan <dipersio.a@gmail.com>
2021-06-01 16:01:40 +0200

    Ajout Starter Project TpCompteBancaireHeritage

---
+++ TpCompteBancaireHeritage/Data/DataBaseConnection.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TpCompteBancaireHeritage.Data
+{
+    public class DataBase
+    {
+        private static string chaine = @"Server=vps748191.ovh.net;User=c0DotNetUser;Password=zD7#ig6P;DataBase=c0DotNetBDD";
+
+        public static MySqlConnection Connection { get => new MySqlConnection(chaine); }
+    }
+}
