beadb5e1d0fe8454b6f12544dd0c8e6155989b99
PetitVulcan <dipersio.a@gmail.com>
2021-06-02 16:27:16 +0200

    Ajout ApiRestAspNet Student

---
+++ ApiRestCRUDStudent/Controllers/StudentController.cs
@@ -0,0 +1,80 @@
+﻿using ApiRestCRUDStudent.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestCRUDStudent.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]    
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger<StudentController> _logger;
+
+        public StudentController(ILogger<StudentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Student>> Get()
+        {
+            return Student.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Student> Get(int id)
+        {
+            return Student.Get(id);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Student s)
+        {
+            if (s.Gender != "" && s.FirstName != "" && s.LastName != "" && s.Email != "" && s.StudentNumber != 0)
+            {
+                if (s.Add())
+                {
+                    return Ok(new { message = "Student added...", error = false });
+                }
+                else
+                {
+                    return Ok(new { message = "Error server...", error = true });
+                }
+            }
+            else
+                return Ok(new { message = "Error server...", error = true });
+        }
+
+        [HttpPut]
+        public ActionResult Update([FromBody] Student s)
+        {
+            if (s.Gender != "" && s.FirstName != "" && s.LastName != "" && s.Email != "" && s.StudentNumber != 0)
+            {
+                if (s.Update())
+                {
+                    return Ok(new { message = "Student Updated...", error = false, s.Id });
+                }
+                else
+                {
+                    return Ok(new { message = "Error server...", error = true });
+                }
+            }
+            else
+                return Ok(new { message = "Error server...", error = true });
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (Student.Delete(id))            
+                return Ok(new { message = "Student Deleted...", error = false, id });
+            else
+                return Ok(new { message = "Error server...", error = true});
+        }
+    }
+}
---
+++ ApiRestCRUDStudent/Models/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiRestCRUDStudent.ModelsDAO;
+
+namespace ApiRestCRUDStudent.Models
+{
+    public class Student
+    {
+        private int id;
+        private string gender;
+        private string firstName;
+        private string lastName;
+        private string email;
+        private int studentNumber;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string gender, string firstName, string lastName, string email, int studentNumber)
+        {
+            Gender = gender;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            StudentNumber = studentNumber;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Email { get => email; set => email = value; }
+        public int StudentNumber { get => studentNumber; set => studentNumber = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public bool Add()
+        {
+            return StudentDAO.AddStudent(this);
+        }
+
+        public static List<Student> Get()
+        {            
+            return StudentDAO.GetStudents();
+        }
+
+        public static Student Get(int id)
+        {
+            return StudentDAO.GetStudent(id);
+        }
+
+        public bool Update()
+        {
+            return StudentDAO.UpdateStudent(this);
+        }
+
+        public static bool Delete(int Id)
+        {
+            return StudentDAO.DeleteStudent(Id);
+        }
+    }
+}
